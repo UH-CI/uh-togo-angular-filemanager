3beeb5338d687827fe5f34f28a75921056ea13c3
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-05-09 12:41:08 -0600

    update workaround to only do extra fileItem listing if system is default
    update workaround to only do extra fileItem listing if system is default

--- src/js/services/filenavigator.js
+++ src/js/services/filenavigator.js
@@ -101,13 +101,19 @@
 
                 FilesController.listFileItems(self.system.id, path, 999999, 0)
                     .then(function (data) {
-                        self.persistUuuids(data).then(function(){
-                          FilesController.listFileItems(self.system.id, path, 999999, 0)
-                              .then(function (data) {
-                                 self.deferredHandler(data, deferred);
-                                 self.requesting = false;
-                               })
-                        })
+                        if (self.system.default){
+                          self.persistUuuids(data).then(function(){
+                            FilesController.listFileItems(self.system.id, path, 999999, 0)
+                                .then(function (data) {
+                                   self.deferredHandler(data, deferred);
+                                   self.requesting = false;
+                                 })
+                          })
+                        }
+                        else{
+                          self.deferredHandler(data, deferred);
+                          self.requesting = false;
+                        }
                     }, function (data) {
                         // need to modify angularjs-sdk to return proper messages here
                         self.error = 'Could not list files for this path';
