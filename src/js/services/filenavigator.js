0ca3be9fd932bf8fa92008a0cf764e46c0e81529
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-05-09 13:13:04 -0600

    call new index agave angular function to get file UUIDs back
    call new index agave angular function to get file UUIDs back

--- src/js/services/filenavigator.js
+++ src/js/services/filenavigator.js
@@ -103,7 +103,7 @@
                     .then(function (data) {
                         if (self.system.default){
                           self.persistUuuids(data).then(function(){
-                            FilesController.listFileItems(self.system.id, path, 999999, 0)
+                            FilesController.indexFileItems(self.system.id, path, 999999, 0)
                                 .then(function (data) {
                                    self.deferredHandler(data, deferred);
                                    self.requesting = false;
